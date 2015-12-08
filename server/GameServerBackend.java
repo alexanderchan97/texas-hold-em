eec7ac6bc8680ab8cfb845c868ac556a09b07920
Alexander Chan <alexanderchan97@gmail.com>
2015-12-07 21:40:35 -0500

    Use backend template code.

--- server/GameServerBackend.java
+++ server/GameServerBackend.java
@@ -1,6 +1,9 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Collections;
@@ -13,6 +16,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import poker.Action;
 import poker.GameModel;
 
 public final class GameServerBackend implements Runnable {
@@ -33,6 +37,7 @@ public final class GameServerBackend implements Runnable {
         if (gm == null || gsm == null) {
             throw new NullPointerException();
         }
+        m = new Model();
         m.setGameModel(gm);
         m.setGameServerModel(gsm);
         taskQueue = new LinkedBlockingQueue<>();
@@ -128,8 +133,9 @@ public final class GameServerBackend implements Runnable {
     }
     
     public void stop() {
+        System.out.println("stopping");
         running = false;
-        if (serverSocket != null && serverSocket.isClosed()) {
+        if (serverSocket != null && !serverSocket.isClosed()) {
             try {
                 serverSocket.close();
             } catch (IOException e) {
@@ -146,7 +152,130 @@ public final class GameServerBackend implements Runnable {
             return;
         }
         
-        Map<Integer, List<String>> serverResponses = broadcast.getResponses();
+        Map<Integer, List<String>> responses = broadcast.getResponses();
+        for (int id : responses.keySet()) {
+            try {
+                Socket clientSocket = openSockets.get(id);
+                PrintWriter pw = new PrintWriter(clientSocket.getOutputStream());
+                for (String response : responses.get(id)) {
+                    pw.println(response);
+                    pw.flush();
+                    System.out.printf("Response sent to user %d: \"%s\"\n",
+                            id, response);
+                }
+                pw.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    private final class ConnectionWorker implements Runnable {
+
+        private final int id;
+        private final Socket clientSocket;
+        BufferedReader reader = null;
+        PrintWriter writer = null;
+        
+        public ConnectionWorker(int id, Socket socket) {
+            this.id = id;
+            this.clientSocket = socket;
+        }
+        
+        @Override
+        public void run() {
+            try {
+                reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                writer = new PrintWriter(clientSocket.getOutputStream(), true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            try {
+                while (running && !clientSocket.isClosed()) {
+                    String line = reader.readLine();
+                    if (line != null) {
+                        if (line.matches("\\d req gmc")) {
+                            writer.print(m.getGameModel().toString());
+                            System.out.println(m.getGameModel().toString());
+                        }
+                        System.out.printf("Request received from user %d: " +
+                                "\"%s\"\n", id, line);
+                        String payload;
+                        if (line.startsWith(":")) {
+                            int index = line.indexOf(' ');
+                            payload = line.substring(index + 1);
+                        } else {
+                            payload = line;
+                        }
+                        Request request = new Request(id, payload);
+                        taskQueue.add(request);
+                    } else {
+                        clientSocket.close();
+                        taskQueue.add(new Disconnection(id));
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+        }
+        
+    }
+    
+    private interface Task {
+        Broadcast getBroadcast();
+    }
+
+    /**
+     * Represents a client's connection to the server.
+     */
+    private final class Registration implements Task {
+        private final int userId;
+
+        public Registration(int userId) {
+            this.userId = userId;
+        }
+
+        @Override
+        public Broadcast getBroadcast() {
+            return m.getGameServerModel().registerUser(userId);
+        }
+    }
+
+    /**
+     * Represents a client's disconnection from the server.
+     */
+    private final class Disconnection implements Task {
+        private final int userId;
+
+        public Disconnection(int userId) {
+            this.userId = userId;
+        }
+
+        @Override
+        public Broadcast getBroadcast() {
+            return m.getGameServerModel().deregisterUser(userId);
+        }
+    }
+
+    /**
+     * Represents an incoming command from a connected client.
+     */
+    private final class Request implements Task {
+        private final int userId;
+        private final String payload;
+
+        public Request(int userId, String payload) {
+            this.userId = userId;
+            this.payload = payload;
+        }
+
+        @Override
+        public Broadcast getBroadcast() {
+            Action action = Action.stringToAction(userId + " " + payload);
+            return null;
+        }
     }
     
 }
